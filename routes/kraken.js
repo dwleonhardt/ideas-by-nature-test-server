8bee2d4a37fb6352f0f641a22663ca41178dbe47
dwleonhardt <dwleonhardt@gmail.com>
2018-04-20 16:41:22 -0600

    add axiom

--- routes/kraken.js
+++ routes/kraken.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', function (req, res) {
-  
+
+  axios.get('https://api.kraken.com/0/public/Assets')
+  .then(response => {
+    console.log(response.data);
+    res.send(response.data)
+  })
+  .catch(error => {
+    console.log(error);
+  });
 })
 
 module.exports = router;
