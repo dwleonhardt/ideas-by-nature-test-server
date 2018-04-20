7874df68595008273fafa7f14e7ab8eab682b2ca
dwleonhardt <dwleonhardt@gmail.com>
2018-04-20 15:40:23 -0600

    add express server and router setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const kraken = require('./routes/kraken');
+
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+app.use('/kraken', kraken);
---
+++ routes/kraken.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/', function (req, res) {
+  
+})
+
+module.exports = router;
